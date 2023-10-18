d4cef94901522d40fcd287db38c8d9702fa99db6
Salman Malik <salman5436@gmail.com>
2023-10-18 00:38:59 -0500

    debugging auth error with provider authentication through nextauth

--- blogr-nextjs-prisma/components/Header.tsx
+++ blogr-nextjs-prisma/components/Header.tsx
@@ -91,7 +91,7 @@ const Header: React.FC = () => {
     right = (
       <div className="right">
         <Link href="/api/auth/signin">
-          <a data-active={isActive('/signup')}>Log in</a>
+          <a data-active={ isActive('/signup')}>Log in</a>
         </Link>
         <style jsx>{`
           a {
